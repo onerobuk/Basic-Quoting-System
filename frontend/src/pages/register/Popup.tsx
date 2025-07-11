ed2998ab4de096d4ed4f0c8f58a70053eb2b85b0
Abhinav Vuppala <abhinav.vuppala@gmail.com>
2025-07-10 20:33:07 -0700

    styling updates

---
+++ frontend/src/pages/register/Popup.tsx
@@ -0,0 +1,34 @@
+import {Dialog} from "@base-ui-components/react";
+import * as React from 'react';
+
+interface popupProps{
+    buttonName:string,
+    modalTitle:string
+    modalContent:string
+}
+
+const Popup = ({buttonName,modalTitle,modalContent}:popupProps) =>{
+
+    return(
+        <Dialog.Root>
+            <Dialog.Trigger className='text-blue-600 underline pr-55'>
+                {buttonName}
+            </Dialog.Trigger>
+            <Dialog.Portal>
+                <Dialog.Backdrop className="fixed inset-0 bg-black opacity-20 transition-all duration-150 dark:opacity-70" />
+                <Dialog.Popup  className="fixed top-1/2 left-1/2 -mt-8 w-96 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-gray-50 p-6 text-gray-900  outline-1 transition-all duration-150">
+                    <Dialog.Title className='font-bold text-black text-xl'>
+                        {modalTitle}
+                    </Dialog.Title>
+                    <Dialog.Description className='py-2'>
+                        {modalContent}
+                    </Dialog.Description>
+                    <Dialog.Close className="border-2 border-black px-2 pb-1 rounded-md hover:bg-black hover:text-white">
+                        Close
+                    </Dialog.Close>
+                </Dialog.Popup>
+            </Dialog.Portal>
+        </Dialog.Root>
+    )
+}
+export default Popup;
--- frontend/src/pages/resources/PartnerGrid.tsx
+++ frontend/src/pages/resources/PartnerGrid.tsx
@@ -2,23 +2,100 @@
 import type {ColDef} from 'ag-grid-community'
 import {AllCommunityModule, ModuleRegistry, themeQuartz} from 'ag-grid-community'
 import {AgGridReact} from "ag-grid-react";
-import {type Dispatch, type SetStateAction, useEffect, useState} from "react";
+import {type Dispatch, type ReactNode, type SetStateAction, useEffect, useState} from "react";
+import Popup from "../register/Popup.tsx";
+import * as React from "react";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 interface partner {
     id:number,
     name:string,
-    isSeller:boolean
+    seller:boolean
+}
+
+interface fullPartner {
+    partnerId:number,
+    partnerName:string,
+    partnerEmail:string,
+    shippingAddress:string,
+    billingAddress:string,
+    seller:boolean
 }
 
 interface partnerGridProps{
     setHeader:Dispatch<SetStateAction<string>>;
 }
 
+const PopupCellRenderer = (props) =>{
+    const [isLoading,setIsLoading] = useState(true)
+    const [partner,setPartner] = useState([]);
+
+    useEffect(() => {
+        getPartnerById(props.value)
+    }, [props.value]);
+
+    if (!props.context?.gridReady) {
+        return null;
+    }
+    
+    async function getPartnerById(id:number){
+        
+        try{
+            const response = await fetch(`http://localhost:8080/partners/${id}`,{
+                method:'GET',
+                headers: {
+                    'Frontend-Key':'tricephalos'
+                }
+            })
+            if(!response.ok){
+                throw new Error(`HTTP ERROR: ${response.status}`)
+            }
+            const data = await response.json();
+            console.log(data);
+            const newPartner:fullPartner = {
+                partnerId:data.partnerId,
+                partnerName:data.partnerName,
+                partnerEmail:data.partnerEmail,
+                shippingAddress:data.shippingAddress,
+                billingAddress:data.billingAddress,
+                seller:data.seller
+            }
+            setPartner(newPartner);
+        } catch (error){
+            console.error('Partner Fetch error: ',error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    console.log('fullPartner');
+    console.log(partner.partnerEmail);
+
+    const partnerDesc:ReactNode =(
+        <span className='grid grid-cols-2'>
+            <label>Id: </label><label>{partner.partnerId}</label>
+            <label>Name: </label><label>{partner.partnerName}</label>
+            <label>Email: </label><label>{partner.partnerEmail}</label>
+            <label>Shipping Address: </label><label>{partner.shippingAddress}</label>
+            <label>Billing Address: </label><label>{partner.billingAddress}</label>
+            <label>Seller: </label><input className="justify-self-start mt-1.5 appearance-none w-4 h-4 border-2 rounded-md" checked={partner.seller} type='checkbox' disabled/>
+        </span>
+    )
+
+    return (
+        <Popup
+            buttonName={props.value}
+            modalTitle={isLoading?'':partner.partnerName}
+            modalContent={isLoading?<label>Loading...</label>:partnerDesc}
+        />
+    )
+}
+
 const PartnerGrid= ({setHeader}:partnerGridProps) => {
     const [partners,setPartners] = useState<partner[]>([]);
     const [isLoading,setIsLoading] = useState(true);
+    const [gridReady,setGridReady] = useState(false);
 
     async function getPartners(){
         try{
@@ -38,7 +115,7 @@ const PartnerGrid= ({setHeader}:partnerGridProps) => {
                 return {
                 id: item.id,
                 name: item.name,
-                isSeller: item.seller
+                seller: item.seller
                 };
         });
             console.log(newPartners);
@@ -51,13 +128,18 @@ const PartnerGrid= ({setHeader}:partnerGridProps) => {
     }
 
     const colDefs: ColDef<partner>[] = [
-        {field: "id"},
+        {
+            field: "id",
+            cellRenderer:PopupCellRenderer,
+        },
         {field: "name"},
-        {field: "isSeller"}
+        {field: "seller"}
     ]
 
     const defaultColDef: ColDef = {
-        flex: 1
+        flex: 1,
+        sortable:true,
+        filter:true
     }
 
     useEffect(() => {
@@ -83,8 +165,10 @@ const PartnerGrid= ({setHeader}:partnerGridProps) => {
 
     return (
         <div className="flex h-screen w-full items-center justify-center bg-neutral-600">
-            <div style={{width: "50%", height: "50%"}}>
+            <div style={{width: "50%", height: "80%"}}>
                 <AgGridReact
+                    onGridReady={()=>setGridReady(true)}
+                    context={{gridReady}}
                     theme={themeQuartz}
                     rowData={partners}
                     columnDefs={colDefs}
